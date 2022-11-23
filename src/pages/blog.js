916e2dd7ee8954092f7ff691a5c01ab3d68892c5
Alex Aguilar <86176019+dan-alcubilla@users.noreply.github.com>
2022-11-22 21:15:15 -0400

    Terminado parte 5 tutorial Gatsby

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,27 +6,32 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
     return(
         <Layout pageTitle="Mis publicaciones de blog">
-            <ul>
             {
-                data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
                 ))
             }    
-            </ul>
         </Layout>
     )
 }
 
 export const query = graphql`
     query {
-        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+        allMdx(sort: {frontmatter: {date: DESC}}) {
             nodes {
-                name
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                }
+                id
+                excerpt
             }
         }
-  }
+    }
 `
 
 export const Head = () => <Seo title="Mis publicaciones de blog" />
